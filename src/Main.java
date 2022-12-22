93c8eb19bc27878355cf8cf3ee1abf6a445a4ffd
Jetzko <andreadefilippi68@gmail.com>
2022-12-22 15:24:51 +0100

    String Handling 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "How are you?";
+
+        char[] array1 = s1.toCharArray();
+        char[] array2 = s2.toCharArray();
+        String char1 = String.copyValueOf(array1, 0, 1);
+        String char2 = String.copyValueOf(array2, 9, 2);
+
+        char[] charArray = char1.concat(char2).toCharArray();
+        System.out.println(charArray);
+    }
+}
