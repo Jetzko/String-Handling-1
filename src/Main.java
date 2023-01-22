e0a242ae2368ad1715b1f0078edbd6ece130c20f
Jetzko <andreadefilippi68@gmail.com>
2023-01-22 15:34:29 +0100

    String Handling 1

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
         char[] array1 = s1.toCharArray();
         char[] array2 = s2.toCharArray();
-        String char1 = String.copyValueOf(array1, 0, 1);
-        String char2 = String.copyValueOf(array2, 9, 2);
+        String char1 = String.copyValueOf(array1, 0, 2);
+        String char2 = String.copyValueOf(array2, 10, 2);
 
         char[] charArray = char1.concat(char2).toCharArray();
         System.out.println(charArray);
